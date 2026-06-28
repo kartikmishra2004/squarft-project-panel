045f37bf932c3a0dd50640e82c8f9cdb582538a1
Kartik Mishra <kartik26nov2004@gmail.com>
2026-06-28 16:13:26 +0530

    Splsh screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,37 @@
-import { Redirect } from "expo-router";
+import { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import { Image } from "expo-image";
+import { StatusBar } from "expo-status-bar";
+import { Redirect, useRouter } from "expo-router";
+
+const SPLASH_DURATION_MS = 1800;
 
 export default function Index() {
+    const router = useRouter();
     const isLoggedIn = false;
+
+    useEffect(() => {
+        if (isLoggedIn) return undefined;
+
+        const splashTimer = setTimeout(() => {
+            router.replace("/(auth)/onboarding1");
+        }, SPLASH_DURATION_MS);
+
+        return () => clearTimeout(splashTimer);
+    }, [isLoggedIn, router]);
 
     if (isLoggedIn) {
         return <Redirect href="/(tabs)/home" />;
     }
 
-    return <Redirect href="/(auth)/onboarding1" />;
+    return (
+        <View className="flex-1 bg-black">
+            <StatusBar hidden />
+            <Image
+                source={require("../assets/images/splash-mobile.gif")}
+                style={StyleSheet.absoluteFill}
+                contentFit="cover"
+            />
+        </View>
+    );
 }
